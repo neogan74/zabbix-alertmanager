d159ff3ccaba60d7a795f7994755a9ea3dfb72b5
Gushchin Andrei <neogan74@gmail.com>
2019-12-17 15:36:28 +0500

    add targets get

--- cmd/zal/main.go
+++ cmd/zal/main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
-	"github.com/devopyio/zabbix-alertmanager/zabbixprovisioner/provisioner"
-	"github.com/devopyio/zabbix-alertmanager/zabbixsender/zabbixsnd"
-	"github.com/devopyio/zabbix-alertmanager/zabbixsender/zabbixsvc"
+	"github.com/neogan74/zabbix-alertmanager/zabbixprovisioner/provisioner"
+	"github.com/neogan74/zabbix-alertmanager/zabbixsender/zabbixsnd"
+	"github.com/neogan74/zabbix-alertmanager/zabbixsender/zabbixsvc"
 	"github.com/povilasv/prommod"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -39,6 +42,8 @@ func main() {
 	provURL := prov.Flag("url", "Zabbix json rpc url.").Envar("ZABBIX_URL").Default("http://127.0.0.1/zabbix/api_jsonrpc.php").String()
 	provKeyPrefix := prov.Flag("key-prefix", "Prefix to add to the trapper item key.").Default("prometheus").String()
 	prometheusURL := prov.Flag("prometheus-url", "Prometheus URL.").Default("").String()
+
+	test := app.Command("test", "Test different things")
 
 	logLevel := app.Flag("log.level", "Log level.").
 		Default("info").Enum("error", "warn", "info", "debug")
@@ -114,6 +119,88 @@ func main() {
 		if err := prov.Run(); err != nil {
 			log.Fatalf("error provisioning zabbix items: %s", err)
 		}
+	case test.FullCommand():
+		//get targets from prom
+		log.Infof("in testing")
+
+		type TargetsList struct {
+			Status string `json:"status"`
+			Data   struct {
+				ActiveTargets []struct {
+					DiscoveredLabels struct {
+						Address     string `json:"__address__"`
+						MetricsPath string `json:"__metrics_path__"`
+						Scheme      string `json:"__scheme__"`
+						Group       string `json:"group"`
+						Job         string `json:"job"`
+					} `json:"discoveredLabels"`
+					Labels struct {
+						Group    string `json:"group"`
+						Instance string `json:"instance"`
+						Job      string `json:"job"`
+					} `json:"labels"`
+					ScrapeURL  string    `json:"scrapeUrl"`
+					LastError  string    `json:"lastError"`
+					LastScrape time.Time `json:"lastScrape"`
+					Health     string    `json:"health"`
+				} `json:"activeTargets"`
+				DroppedTargets []interface{} `json:"droppedTargets"`
+			} `json:"data"`
+		}
+
+		/*
+			//prom query
+			client, err := api.NewClient(api.Config{
+				Address: "http://51.15.213.9:9090",
+			})
+			if err != nil {
+				log.Fatalf("Error in request %s", err)
+				os.Exit(1)
+			}
+
+			v1api := v1.NewAPI(client)
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			result, err := v1api.Query(ctx, "up", time.Now())
+			if err != nil {
+				log.Fatalf("Error in quering to Prometheus: %s", err)
+				os.Exit(1)
+			}
+			data, err := result.Type().MarshalJSON()
+			if err != nil {
+				log.Fatalf("Error whil unmarshal json from result", err)
+			}
+
+			m := string(data[:])
+
+			log.Infof("Result:\n %v\n", m)
+			log.Infof("Result:\n %v\n", result.String())
+		*/
+
+		resp, err := http.Get("http://51.15.213.9:9090/api/v1/targets")
+		if err != nil {
+			log.Infof("Error while get targets: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Error with body: %v\n", err)
+		}
+		// log.Infof("Targets: %v\n", data)
+
+		var targets TargetsList
+		err = json.Unmarshal(data, &targets)
+		if err != nil {
+			log.Fatalf("Error while JSON unmarshal %s", err)
+		}
+		log.Infof("Targets: %v\n", targets)
+		for _, v := range targets.Data.ActiveTargets {
+
+			log.Infof("%v\n", v.Labels.Instance[:strings.LastIndex(v.Labels.Instance, ":")])
+		}
+
 	}
 }
 
